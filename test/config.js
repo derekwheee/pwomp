7b8c5084d125ae8b0f7f9cad9f9231a35511ad77
Derek Wheelden <derek.wheelden@gmail.com>
2017-09-10 15:02:13 -0400

    Begin adding unit tests

---
+++ test/config.js
@@ -0,0 +1,20 @@
+const test = require('ava');
+const path = require('path');
+const config = require('../lib/config');
+
+test('loaded', t => {
+	t.true(config && 'SETTINGS' in config);
+});
+
+test('get path', t => {
+    t.is(
+        config.getPath('viewsDir'),
+        path.resolve((path.join(process.cwd(), config.SETTINGS.srcDir, config.SETTINGS.viewsDir)))
+    );
+});
+
+test.skip('load .exemplarrc', t => {
+    // save temp copy of .exemplarrc
+    // write test copy of .exemplarrc
+    // test loaded values
+});
---
+++ test/util.js
@@ -0,0 +1,8 @@
+const test = require('ava');
+const path = require('path');
+const util = require('../lib/util');
+
+test('normalize path', t => {
+    const testPath = 'test/path.txt';
+	t.is(util.normalizePath(testPath), path.resolve(testPath));
+});
